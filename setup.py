ac0b49904794c66087448b4be2d168754d4e3b0e
krishi <patel.23@iitj.ac.in>
2024-05-26 14:04:50 +0530

    setup script added and integrated with requirements.txt

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+REQUIREMENTS_PATH = "requirements.txt"
+TRIGGER = "-e ."
+
+def get_requirements(file_path : str) -> List[str]:
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if TRIGGER in requirements:
+            requirements.remove(TRIGGER)
+    return requirements
+
+setup(
+    name = 'end-to-end-DS',
+    version = '3.11.9',
+    author = 'Krishi Patel',
+    packages = find_packages(),
+    install_requires = get_requirements(REQUIREMENTS_PATH)
+
+)
